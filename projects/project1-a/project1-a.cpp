4463a23f02797d6f93d25c6c7b805fbbfb46f5e0
Deny Vugdalic <dvugdalic001@student.butte.edu>
2018-02-07 03:18:00 +0000

    Luhn Algorithm start

--- projects/project1-a/project1-a.cpp
+++ projects/project1-a/project1-a.cpp
@@ -12,6 +12,20 @@
 #include<sstream>
 using namespace std;
 
+
+/*void LuhnAlg(string ccNum){
+    int sum = 0;
+    int digit = length(ccNum);
+    int mod2 = digit%2;
+    for(i = 0; i < digit.length()-1)
+        int digit = int[i];
+    if(i%2 = mod2)
+        digit = digit * 2;
+    if(digit > 9)
+        digit = digit - 9;
+    sum = sum + digit;
+    }
+*/
 int main(){
     ifstream stream; //input file stream
     string ccNum; //string of credit card numbers
@@ -19,18 +33,18 @@ int main(){
     
     stream.open("creditcardnumbers.txt");
     if(!stream.is_open()){
-        cout << "Could not open file" << endl;
+        cout << "Error: could not open file" << endl;
         return 1;
     }
         
     while(getline (stream, ccNum)){ //while loop to read in the numbers
         ss << ccNum;
         cout << ccNum << endl;
-    }
-    stream.close(); //close file when done
     
 
+    stream.close(); //close file when done
+    
     return 0;
- 
 
+}
 }
