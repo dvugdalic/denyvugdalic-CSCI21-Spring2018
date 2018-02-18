314119e8cc27cb6e107f6c1f7fda98a383dd004b
Deny Vugdalic <dvugdalic001@student.butte.edu>
2018-02-18 07:50:50 +0000

    Pets lab - start

---
+++ labwork/inheritance/pets.cpp
@@ -0,0 +1,63 @@
+#include<string>
+
+using std::string;
+
+ 
+
+class Pet
+
+{
+
+Public:
+
+                //default constructor for pet that defaults all values to 0, false or blank.
+
+                Pet();
+
+                //write appropriate overloaded constructors
+
+                //write get and set functions for each variable
+
+                void print();
+
+protected:
+
+                string name_;
+
+                int species_;
+
+                bool nueteredSpayed_;
+
+                bool talks_;
+
+};
+
+Class Dog
+
+{
+    Public:
+    
+    protected:
+    
+}
+
+Class Cat
+{
+    Public:
+    
+    protected:
+}
+
+Class Bird
+{
+    Public:
+    
+    protected:
+}
+
+int main(){
+    Pet p;
+    Dog d;
+    Cat c;
+    Bird b;
+}
