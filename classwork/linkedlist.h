11aacb0aed3a72cb0160ecd35a42c3ec459c7de5
Deny Vugdalic <dvugdalic001@student.butte.edu>
2018-03-28 03:05:54 +0000

    added linked list work

---
+++ classwork/linkedlist.h
@@ -0,0 +1,48 @@
+/*
+ * Name        : linkedlist.h
+ * Author      : Deny Vugdalic
+ * Description : Class Header File
+ */
+ 
+ #include<iostream>
+ using namespace std;
+ 
+ #ifndef LINKEDLIST_H
+ #define LINKEDLIST_H
+ 
+ class Student {
+     private:
+        string firstName;
+        string lastName;
+        string id;
+        string status;
+        string unitsCompleted;
+     public:
+        Student(); //constructor
+        //destructor
+        //get
+        //set
+ };
+ 
+ class Node {
+     private:
+        
+     public:
+        Node(); //constructor
+        //thing
+        //next = null
+        //destructor
+        //delete next
+        //next = null
+        //constructor(thing)
+        //next = null
+        //setNext(address)
+        //next = address
+        //getNext()
+        //return *next
+        //getThing()
+        
+        
+ };
+ 
+ #endif
