158ac5bf5b2f65362d0e0fdc406c50df10e66221
Deny Vugdalic <dvugdalic001@student.butte.edu>
2018-05-21 05:17:14 +0000

    project 3

--- projects/project3/predictivetext.h
+++ projects/project3/predictivetext.h
@@ -18,13 +18,41 @@ class Autocomplete{
         Autocomplete();
         ~Autocomplete();
         string string;
+        string prefix;
         float weight;
     private:
-        void ascendingOrder();
-        void descendingOrder();
-        void ascendingR();
+        void ascendingOrder(float weight);
+        void descendingOrder(float weight);
+        void ascendingR(float weight);
+        void binarySearch(int arr[], int begin, int end, int x);
+        void autoComplete();
 };
 
-void ascendingOrder(){
+void ascendingOrder(float weight){
+    
+}
+
+void descendingOrder(float weight){
+    
+}
+
+void ascending(float weight){ //ascending for r characters
+
+}
+
+void binarySearch(int arr[], int begin, int end, int x){// binary search algorithm
+    if (end >= 1){
+        int middle = 1 + (end - 1)/2; //middle is equal to the beginning plus the ending minus 1 all divided by 2
+    if (arr[middle] == x) //if element is in the middle of the string
+        return middle; //return the element
+    if (arr[middle] > x) //smaller than middle = left
+        return binarySearch(int arr,int begin, int mid-1, int x); //recursive call
+        return binarySearch(int arr, int mid+1, int x); //recursive call
+    }
+    else
+        return 1; //element is not in the array
+}
+
+void autoComplete(){
     
 }
